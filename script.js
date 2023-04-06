f2cbdbc2f3de3ad3b5b10c8d0713ca07705a949d
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-04-06 12:48:13 -0700

    added reset functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const divClassName = 'grid-div';
 const gridContainer = document.querySelector('.sketch-pad');
 const body = document.querySelector('body');
+const reset = document.querySelector('.reset');
 
 function createGrid(width) {
   for (let i = 1; i <= width ** 2; i += 1) {
@@ -11,21 +12,9 @@ function createGrid(width) {
   }
 }
 
-// hard code this for now but the idea is too call this every
-// time the user changes the size of the pad
 createGrid(16);
 
-// add an event listener that takes a click as an action and
-// changes the background color of whatever div was targeted
-// for now just set the background color to black
-// get all elements that fall under the specified class
 const gridDivs = document.querySelectorAll(`.${divClassName}`);
-
-// iterate through node list to apply event listener
-// need to handle drag vs. click
-// we can set a variable to false and when we clock a div it sets the drag to True
-// when clicked or dragging with the var == true then change the background color
-// when mouse comes up set the drag var to false
 
 function dragDraw(e) {
   document.getElementById(e.target.id).setAttribute('style', 'background-color: black;');
@@ -45,5 +34,11 @@ gridDivs.forEach((element) => {
     gridDivs.forEach((div) => {
       div.addEventListener('mousemove', dragDraw);
     });
+  });
+});
+
+reset.addEventListener('click', () => {
+  gridDivs.forEach((element) => {
+    element.setAttribute('style', 'background-color: white;');
   });
 });
