b001991454ab67d49dd0f288d228994b42a64e82
Hunter Evangelista <hunterjevangelista@gmail.com>
2023-04-06 14:17:58 -0700

    added random color functionality

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const divClassName = 'grid-div';
 const gridContainer = document.querySelector('.sketch-pad');
 const body = document.querySelector('body');
 const reset = document.querySelector('.reset');
+const randomColorButton = document.querySelector('.random-color');
+let randColorSetting = false;
+let paintColor = 'rgba(0, 0, 0, 1)'; // deafault color
 
 function createGrid(width) {
   for (let i = 1; i <= width ** 2; i += 1) {
@@ -16,10 +19,46 @@ createGrid(16);
 
 const gridDivs = document.querySelectorAll(`.${divClassName}`);
 
-function dragDraw(e) {
-  document.getElementById(e.target.id).setAttribute('style', 'background-color: black;');
+// use below function as dynamic brush choices
+
+reset.addEventListener('click', () => {
+  gridDivs.forEach((element) => {
+    element.setAttribute('style', 'background-color: white;');
+  });
+});
+
+// create random color capability
+// generate three different integers between 0 and 255
+function randomInt(limit) {
+  return Math.floor(Math.random() * (limit + 1));
 }
 
+function randomPaintColor() {
+  // generate random rgb color
+  return `rgba(${randomInt(256)}, ${randomInt(256)}, ${randomInt(256)}, 1)`;
+}
+
+function handleRandomColorClick() {
+  randomColorButton.classList.toggle('clicked');
+  if (randColorSetting === true) {
+    randColorSetting = false;
+  } else if (randColorSetting === false) {
+    randColorSetting = true;
+  }
+}
+
+function setBrushColor() {
+  if (randColorSetting) {
+    paintColor = randomPaintColor();
+  } else {
+    paintColor = 'rgb(0, 0, 0)';
+  }
+}
+
+function dragDraw(e) {
+  setBrushColor();
+  document.getElementById(e.target.id).setAttribute('style', `background-color: ${paintColor};`);
+}
 function stopDragDraw() {
   gridDivs.forEach((element) => {
     element.removeEventListener('mousemove', dragDraw);
@@ -30,15 +69,19 @@ body.addEventListener('mouseup', stopDragDraw);
 
 gridDivs.forEach((element) => {
   element.addEventListener('mousedown', () => {
-    element.setAttribute('style', 'background-color: black;');
+    element.setAttribute('style', `background-color: ${paintColor};`);
     gridDivs.forEach((div) => {
       div.addEventListener('mousemove', dragDraw);
     });
   });
 });
 
-reset.addEventListener('click', () => {
-  gridDivs.forEach((element) => {
-    element.setAttribute('style', 'background-color: white;');
-  });
-});
+// how do we get this inserted into the initial draw process?
+// this function needs to fire off everytime a color is applied
+// different brush functionality could be a toggle
+// click random color
+// button gets a new class applied to it
+// class gives a visual indication it was clicked
+// can do this with a variable that is true or false indicating its state
+
+randomColorButton.addEventListener('click', handleRandomColorClick);
